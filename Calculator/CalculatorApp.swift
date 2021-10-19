afd2bddd56d5ee3426ce84f0de5a1fbe7305a042
Tinashe "Viz" Matembo <31240908+Vizz03@users.noreply.github.com>
2021-10-19 22:32:32 +0200

    SwiftUI Calculator

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tinashe Matembo on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
