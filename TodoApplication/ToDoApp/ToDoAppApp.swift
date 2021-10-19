fcf85dc15728b2f2faa98c7593d056c79ff9bb62
Tinashe "Viz" Matembo <31240908+Vizz03@users.noreply.github.com>
2021-10-20 01:42:36 +0200

    Todo Application in SwiftUI

---
+++ TodoApplication/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Tinashe Matembo on 22/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LandingView()
+        }
+    }
+}
---
+++ TodoApplication/ToDoApp/ViewModel/NewTaskViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  NewTaskViewModel.swift
+//  ToDoApp
+//
+//  Created by Tinashe Matembo on 25/07/21.
+//
+
+import Foundation
+import SwiftUI
+
+class NewTaskViewModel: ObservableObject{
+    
+    
+    
+    
+}
